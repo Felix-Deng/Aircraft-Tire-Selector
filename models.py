016a80b7607afead9f34df0e8b50870e083585e1
Felix-Deng <felixdeng102@gmail.com>
2023-07-16 21:32:30 -0400

    Style cleanup

--- models.py
+++ models.py
@@ -6,10 +6,12 @@ class Tire:
     """
     
     def __init__(self, **kwargs) -> None:
+        """Initiate a Tire object to store all parameters of an aircraft tire. 
         
-        self.Pre: str = kwargs.get('Pre', 0)
-        
+        kwargs include all parameters given from manufacturer's databook. 
+        """
         # Tire Descriptions
+        self.Pre: str = kwargs.get('Pre', '')
         self.M: float = kwargs.get('M', 0)
         self.N: float = kwargs.get('N', 0)
         self.D: float = kwargs.get('D', 0)
@@ -24,8 +26,10 @@ class Tire:
         # Inflated Tire Dimensions (inches)
         self.DoMax: float = kwargs.get('DoMax', 0)
         self.DoMin: float = kwargs.get('DoMin', 0)
+        self.Dm = (self.DoMax + self.DoMin) / 2     # mean tire diameter 
         self.WMax: float = kwargs.get('WMax', 0)
         self.WMin: float = kwargs.get('WMin', 0)
+        self.Wm = (self.WMax + self.WMin) / 2       # mean overall tire width 
         self.DsMax: float = kwargs.get('DsMax', 0)
         self.WsMax: float = kwargs.get('WsMax', 0)
         
@@ -38,147 +42,106 @@ class Tire:
         
         # Rim Description (inches)
         self.A: float = kwargs.get('A', 0)
-        self.D2: float = kwargs.get('D2', 0)                # specified rim diameter
+        self.RD: float = kwargs.get('RD', 0)
         self.FH: float = kwargs.get('FH', 0)
         self.G: float = kwargs.get('G', 0)
-        self.DF: float = kwargs.get('DF', 0)                # outer flange diameter
+        self.DF: float = kwargs.get('DF', 0)
         
         # Qualification Standard
-        self.QS: str = kwargs.get('QS', 0)
+        self.QS: str = kwargs.get('QS', '')
 
-    def max_load_capacity_calc(self) -> float: 
-        """Calculate the maximum loading capacity of the tire.
+    def max_load_capacity(self, exact=False) -> float: 
+        """
+        Args:
+            exact (bool, optional): should the exact load be returned? Defaults to False.
 
         Returns:
-            float: maximum loading capacity 
+            float: maximum loading capacity of the tire in lbs; 
+                   returned value rounded to the nearest 25 pounds if not exact. 
         """
-        
-        Ad = self.ground_contact_area_calc()                # ground contact area at d in in^2
-        P = self.pressure_index_calc()                      # pressure index
-        Pc = self.load_supporting_capability_calc()         # load supporting capability of the tire carcass in units of equivalent pressure
-        
-        # print(f'{Ad},    {P},    {Pc}')
-        Lm = Ad * (P + Pc)                                  # maximum load capacity in lbs
-        Lm = round(Lm / 25) * 25
+        Lm = self.ground_contact_area() * (self.pressure_index() + self.load_supporting_capability()) 
+        if exact: 
+            return Lm 
+        else: 
+            return round(Lm / 25) * 25 
     
-        return Lm
-    
-    def ground_contact_area_calc(self) -> float:
-        """Calculate the ground contact area of the tire.
+    def ground_contact_area(self, b=0.32) -> float:
+        """
+        Args:
+            b (float, optional): fractional tire deflection. 
+                                 Defaults to 0.32 for type VII tires.
 
         Returns:
-            float: ground contact area (Ad)
+            float: ground contact area of the tire (Ad)
         """
-        d = self.actual_deflection_calc()       # actual deflection
-        Dm = (self.DoMax + self.DoMin)/2        # mean tire diameter
-        Wm = (self.WMax + self.WMin)/2          # mean overall tire width
-        Ad = 0.77 * math.pi * d * math.sqrt((Dm - d) * (Wm - d))
-        
-        return Ad
+        d = b * (self.Dm - self.DF)/2  # actual deflection of the tire 
+        return 0.77 * math.pi * d * math.sqrt((self.Dm - d) * (self.Wm - d))
     
-    def actual_deflection_calc(self) -> float:
-        """Calculate the actual deflection of the tire.
+    def pressure_index(self, T0=4.4) -> float:
+        """
+        Args:
+            T0 (float, optional): 4 for type III tires; defaults to 4.4 for others.
 
         Returns:
-            float: actual deflection (d)
+            float: pressure index of the tire (P)
         """
-        
-        Dm = (self.DoMax + self.DoMin)/2        # mean tire diameter
-        b = 0.32                                # fractional tire deflection, see Table 1
-        d = b * (Dm - self.DF)/2    
-
-        return d
-    
-    def pressure_index_calc(self) -> float:
-        """Calculate the pressure index of the tire.
-
-        Returns:
-            float: pressure index (p)
-        """
-        
-        # ratio of ends per inch
-        Re = self.ratio_of_ends_per_inch_calc()
-        
-        # constants
-        T0 = 4.4                 # 4 for type III tires and 4.4 for all other tires, since we only consider type III tire, 4.4 used for calculation
+        Re = self.ratio_of_ends_per_inch()
         Ne = self.PR - 0.4
-        
-        Dm = (self.DoMax + self.DoMin)/2        # mean tire diameter
-        a = (Dm - self.D)/4
-        Q = 2.5 + self.D/(2 * Dm)
+        a = (self.Dm - self.D)/4
+        Q = 2.5 + self.D / (2 * self.Dm)
         S = a * Q
-        
-        # operating factor
-        F0 = (-1.623104 * 10 ** (-7) * self.D ** 5) + (1.463062 * 10 ** (-5) * self.D ** 4) - \
-              (5.607522 * 10 ** (-4) * self.D ** 3) + (0.01288401 * self.D ** 2) - (0.197904 * self.D) + 2.567982 
-
-        
-        P = (40 * Re * T0 * Ne) / (S * F0)
-
-        return P
+        F0 = (
+            (-1.623104 * 10**(-7) * self.D**5) \
+            + (1.463062 * 10**(-5) * self.D**4) \
+            - (5.607522 * 10**(-4) * self.D**3) \
+            + (0.01288401 * self.D**2) \
+            - (0.197904 * self.D) \
+            + 2.567982
+        )
+        return (40 * Re * T0 * Ne) / (S * F0)
     
-    def ratio_of_ends_per_inch_calc(self) -> float:
-        """Calculate the ratio of ends per inch of the tire.
-
-        Returns:
-            float: ratio of ends per inch (Re)
+    def ratio_of_ends_per_inch(self) -> float:
         """
-        
-        Dm = (self.DoMax + self.DoMin)/2        # mean tire diameter
-        Lr = Dm/self.D                          # Lift Ratio
-        
+        Returns:
+            float: ratio of ends per inch of the tire (Re)
+        """
+        Lr = self.Dm/self.D  # lift ratio
         if 1.5 < Lr < 2.2:
-            Re = 1.475 - 0.331 * Lr
+            return 1.475 - 0.331 * Lr
         elif 2.2 <= Lr < 5:
-            Re = (-0.007651 * Lr ** 5) + (0.14362 * Lr ** 4) - (1.0668308 * Lr ** 3) + \
-                   (3.9519228 * Lr ** 2) - (7.4168297 * Lr) + 6.3261135
+            return (
+                -0.007651 * Lr**5 \
+                + 0.14362 * Lr**4 \
+                - 1.0668308 * Lr**3 \
+                + 3.9519228 * Lr**2 \
+                - 7.4168297 * Lr \
+                + 6.3261135
+            )
         else:
             raise ValueError("Lr value is outside the defined range.")
-        
-        return Re
     
-    def load_supporting_capability_calc(self) -> float:
-        """Calculate the load supporting capability of the tire carcass in units of equivalent pressure.
-
-        Returns:
-            float: load supporting capability (Pc)
+    def load_supporting_capability(self) -> float:
         """
-        
-        Dm = (self.DoMax + self.DoMin)/2        # mean tire diameter
-        Wm = (self.WMax + self.WMin)/2          # mean overall tire width
-        
-        # assume the mean cross section == mean tire width for now, but it is probably not miss a length
-        if Dm < 5.5:
-            Pc = self.N
+        Returns:
+            float: load supporting capability of the tire carcass 
+                   in units of equivalent pressure (Pc)
+        """
+        # assume the mean cross section == mean tire width 
+        if self.Dm < 5.5:
+            return self.N
         else:
-            Pc = (10.4 * self.PR ** 2) / Wm ** 2
-        
-        return Pc
-        
-
-############
-# read csv
-    @classmethod
-    def from_row(cls, row):
-        row_data = {}
-        for key, value in zip(cls.header(), row):
-            try:
-                row_data[key] = float(value)
-            except ValueError:
-                row_data[key] = None  # Assign a default value if conversion fails
-        return cls(**row_data)
-
-    @staticmethod
-    def header():
-        return ['Pre','M', 'N', 'D', 'PR', 'SI', 'Lm', 'IP', 'BL', 'DoMax', 'DoMin', 'WMax', 'WMin', 'DsMax',
-                'WsMax', 'AR', 'LR_RL', 'LR_BL', 'A', 'D2', 'FH', 'G', 'DF', 'QS']
+            return (10.4 * self.PR ** 2) / self.Wm ** 2
 
 
-
-
-'''
 if __name__ == "__main__": 
-    ###### Test run this file to make sure the max loading capacity is printed ######
-    tire = Tire() # Fill in the parameters required 
-    print(tire.max_load_capacity_calc())
-'''
+    # Test run the max loading capacity calculation 
+    tire = Tire(
+        Pre='', M=21, N=7.25, D=10, PR=10, SI=210, Lm=5150, IP=135, BL=15450, DoMax=21.25, DoMin=20.60,
+        WMax=7.20, WMin=6.80, DsMax=19.25, WsMax=6.35, AR=0.78, LR_RL=9, LR_BL=6.8, A=5.50, RD=10,
+        FH=1, G=1.4, DF=12, QS='TSO-C62'
+    )
+    print(
+        f'From Databook: {tire.Lm}\n'\
+        + f'From Calculation (rounded): {tire.max_load_capacity()}\n'\
+        + f'From calculation (exact): {round(tire.max_load_capacity(exact=True), 2)}'
+    )
