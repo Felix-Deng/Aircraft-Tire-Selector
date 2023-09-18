401b4a322c7f6439591a9a2289a2c1669c9ebfbe
Felix-Deng <felixdeng102@gmail.com>
2023-09-17 22:12:39 -0400

    Update xDSM plot

--- XDSM/XDSM_plotter.py
+++ XDSM/XDSM_plotter.py
@@ -3,18 +3,22 @@ from pyxdsm.XDSM import XDSM, OPT, SOLVER, FUNC, LEFT
 x = XDSM(use_sfmath=True)
 
 x.add_system("opt", OPT, r"\text{Optimizer}")
-x.add_system("geo", FUNC, r"\text{Geometry}")
+x.add_system("solver", SOLVER, r"\text{Geometry}")
+x.add_system("load", FUNC, "L_m(x)")
+x.add_system("mass", FUNC, r"m_{tire}(x)")
 x.add_system("mech", FUNC, r"\text{Mechanical Feasibility}")
 
-x.connect("opt", "geo", "x")
-x.connect("geo", "mech", "x")
+x.connect("opt", "solver", "x")
+x.connect("solver", "load", "x")
+x.connect("solver", "mass", "x")
+x.connect("solver", "mech", "x")
 
-# x.connect("geo", "opt", "x^*")
-x.connect("mech", "opt", "x^*")
+x.connect("load", "opt", "L_m")
+x.connect("mass", "opt", "m_{tire}")
+x.connect("mech", "opt", "\sigma_{cord}")
 
 x.add_input("opt", "x^{(0)}")
+x.add_input("mech", "I_{speed}")
 x.add_output("opt", "x^*", side=LEFT)
-x.add_output("geo", "L_m", side=LEFT)
-# x.add_output("mech", "\sigma_{fibre}", side=LEFT)
 
 x.write("mdf", outdir="./XDSM")
