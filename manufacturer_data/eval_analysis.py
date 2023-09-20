8d1d8b7ed69403eddf321756651ca6227d78ece2
Felix-Deng <felixdeng102@gmail.com>
2023-09-19 21:19:42 -0400

    Calculate standard error for model

--- manufacturer_data/eval_analysis.py
+++ manufacturer_data/eval_analysis.py
@@ -1,5 +1,6 @@
 import pandas as pd 
 import statsmodels.api as sm 
+from scipy.stats import linregress
 import matplotlib.pyplot as plt 
 
 df = pd.read_csv("manufacturer_data/eval_results.csv", date_format=float)
@@ -20,6 +21,12 @@ print(results.summary())
 # Sort by scale of error 
 sorted_df = df.sort_values(by=["Absolute Relative Difference"], ascending=False)
 sorted_df.to_csv("manufacturer_data/sorted_eval.csv")
+
+# Print standard error between manufacturer Lm and calculated Lm (rounded)
+print()
+err_df = df[['Lm', 'Rounded Calc Lm']].dropna()
+print(f"Included tires: {len(err_df)}/{len(df)}")
+print("Standard error:", linregress(err_df['Lm'], err_df['Rounded Calc Lm']).stderr)
 
 # Plot relationship between individual parameter 
 param = "Lm"
