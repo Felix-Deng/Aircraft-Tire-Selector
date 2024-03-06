08eb5390305dba2df4f8f2c7470a8850f8198ec7
szuling <szuling.liu@mail.utoronto.ca>
2024-03-06 15:36:03 -0500

    Minor UI change

--- main.py
+++ main.py
@@ -494,7 +494,7 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "Tire Optimizer"))
         self.groupBox.setTitle(_translate("MainWindow", "Design Variables"))
         self.label_12.setText(_translate("MainWindow", "Dm: mean tire diameter"))
         self.label_13.setText(_translate("MainWindow", "DF: outer wheel flange diameter"))
@@ -751,7 +751,7 @@ class Ui_MainWindow(object):
         input_summary = f"""<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0//EN" "http://www.w3.org/TR/REC-html40/strict.dtd">
         <html><head><meta name="qrichtext" content="1" /><style type="text/css">
         p, li {{ white-space: pre-wrap; }}
-        </style></head><body style=" font-family:'.AppleSystemUIFont'; font-size:14pt; font-weight:400; font-style:normal;">"""
+        </style></head><body style=" font-family:'.AppleSystemUIFont'; font-size:11pt; font-weight:400; font-style:normal;">"""
         if self.unit == "metric": 
             input_summary += "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><em>Note: optimization requirements were entered in metric units but converted and displayed in imperial units.</em></p><br />"
         
